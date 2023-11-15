98c7ba98021f11faad8656d5f45bb1404bb50e90
sinalpha <hanhdhx65@gmail.com>
2023-11-15 15:01:27 +0900

    fix bugs

--- First.cpp
+++ First.cpp
@@ -1,48 +1,96 @@
 #include <windows.h>
 
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
 HINSTANCE g_hInst;
-LPCTSTR lpszClass = TEXT("First");
+LPCTSTR lpszClass=TEXT("First");
 
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
-
+int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance
+		  ,LPSTR lpszCmdParam,int nCmdShow)
+{
 	HWND hWnd;
 	MSG Message;
 	WNDCLASS WndClass;
-	g_hInst = hInstance;
-
-	WndClass.cbClsExtra = 0;
-	WndClass.cbWndExtra = 0;
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	WndClass.hInstance = hInstance;
-	WndClass.lpszClassName = lpszClass;
-	WndClass.lpszMenuName = NULL;
-	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	g_hInst=hInstance;
+	
+	WndClass.cbClsExtra=0;
+	WndClass.cbWndExtra=0;
+	WndClass.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor=LoadCursor(NULL,IDC_ARROW);
+	WndClass.hIcon=LoadIcon(NULL,IDI_APPLICATION);
+	WndClass.hInstance=hInstance;
+	WndClass.lpfnWndProc=WndProc;
+	WndClass.lpszClassName=lpszClass;
+	WndClass.lpszMenuName=NULL;
+	WndClass.style=CS_HREDRAW | CS_VREDRAW;
 	RegisterClass(&WndClass);
 
-	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, (HMENU)NULL, hInstance, NULL);
-	ShowWindow(hWnd, nCmdShow);
-
-	while (GetMessage(&Message, NULL, 0, 0)) {
+	hWnd=CreateWindow(lpszClass,lpszClass,WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,
+		NULL,(HMENU)NULL,hInstance,NULL);
+	ShowWindow(hWnd,nCmdShow);
+	
+	while (GetMessage(&Message,NULL,0,0)) {
 		TranslateMessage(&Message);
 		DispatchMessage(&Message);
 	}
-
 	return (int)Message.wParam;
-
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
+{
 	switch (iMessage) {
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
 	}
-
-	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+	return(DefWindowProc(hWnd,iMessage,wParam,lParam));
 }
+
+
+//#include <windows.h>
+//
+//LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//HINSTANCE g_hInst;
+//LPCTSTR lpszClass = TEXT("First");
+//
+//int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+//	, LPSTR lpszCmdParam, int nCmdShow)
+//{
+//	HWND hWnd;
+//	MSG Message;
+//	WNDCLASS WndClass;
+//	g_hInst = hInstance;
+//
+//	WndClass.cbClsExtra = 0;
+//	WndClass.cbWndExtra = 0;
+//	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+//	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+//	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+//	WndClass.hInstance = hInstance;
+//	WndClass.lpfnWndProc = WndProc;
+//	WndClass.lpszClassName = lpszClass;
+//	WndClass.lpszMenuName = NULL;
+//	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+//	RegisterClass(&WndClass);
+//
+//	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+//		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+//		NULL, (HMENU)NULL, hInstance, NULL);
+//	ShowWindow(hWnd, nCmdShow);
+//
+//	while (GetMessage(&Message, NULL, 0, 0)) {
+//		TranslateMessage(&Message);
+//		DispatchMessage(&Message);
+//	}
+//	return (int)Message.wParam;
+//}
+//
+//LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+//{
+//	switch (iMessage) {
+//	case WM_DESTROY:
+//		PostQuitMessage(0);
+//		return 0;
+//	}
+//	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+//}
