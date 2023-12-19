3750c30b4ee3192b256d1fcf92e18575e977fc40
ssinalpha <hanhdhx65@gmail.com>
2023-12-20 01:59:22 +0900

    3.3 완료

---
+++ src/MessageBox.cpp
@@ -0,0 +1,66 @@
+#include <windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("DrawText");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance
+	, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+
+
+	TCHAR buffer[128];
+	//GetWindowText(hWnd, buffer, sizeof(buffer) / (sizeof TCHAR));
+
+	
+
+	switch (iMessage) {
+	case WM_LBUTTONDOWN:	
+		
+		if (MessageBox(hWnd, TEXT("마우스 왼쪽 버튼을 눌렀습니다."), TEXT("메시지 박스"), MB_OKCANCEL | MB_ICONERROR) == IDOK) {
+			MessageBeep(MB_OK);
+			MessageBox(hWnd, TEXT("OK를 눌렀습니다"), TEXT("메시지 박스"), MB_OK);
+			SetWindowText(hWnd, TEXT("Hello!"));
+		}
+
+		
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
