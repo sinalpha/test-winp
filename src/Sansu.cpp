b54059127f7e52f26581e7b43ec63c88b72c08c2
ssinalpha <hanhdhx65@gmail.com>
2023-12-24 21:56:55 +0900

    4-3-다 완성

--- src/Sansu.cpp
+++ src/Sansu.cpp
@@ -1,74 +1,74 @@
-#include <windows.h>
-#include <random>
-
-std::uniform_int_distribution<int> uid{-29, 29};
-std::default_random_engine dre;
-
-LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-HINSTANCE g_hInst;
-LPCTSTR lpszClass = TEXT("Timer");
-
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
-	, LPSTR lpszCmdParam, int nCmdShow)
-{
-	HWND hWnd;
-	MSG Message;
-	WNDCLASS WndClass;
-	g_hInst = hInstance;
-
-	WndClass.cbClsExtra = 0;
-	WndClass.cbWndExtra = 0;
-	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	WndClass.hInstance = hInstance;
-	WndClass.lpfnWndProc = WndProc;
-	WndClass.lpszClassName = lpszClass;
-	WndClass.lpszMenuName = NULL;
-	WndClass.style = CS_HREDRAW | CS_VREDRAW;
-	RegisterClass(&WndClass);
-
-	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-		NULL, (HMENU)NULL, hInstance, NULL);
-	ShowWindow(hWnd, nCmdShow);
-
-	while (GetMessage(&Message, NULL, 0, 0)) {
-		TranslateMessage(&Message);
-		DispatchMessage(&Message);
-	}
-	return (int)Message.wParam;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
-{
-	HDC hdc;
-	PAINTSTRUCT ps;
-	static RECT rt = { 100,100,400,120 };
-	static TCHAR sTime[128];
-	static int a;
-	static int b;
-
-	switch (iMessage) {
-	case WM_CREATE:
-		SetTimer(hWnd, 1, 3000, NULL);
-		SendMessage(hWnd, WM_TIMER, 1, 0);
-		return 0;
-	case WM_TIMER:
-		a = uid(dre);
-		b = uid(dre);
-		wsprintf(sTime, TEXT("%d + %d = ?"), a, b);
-		InvalidateRect(hWnd, &rt, TRUE);
-		return 0;
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		TextOut(hdc, 100, 100, sTime, lstrlen(sTime));
-		EndPaint(hWnd, &ps);
-		return 0;
-	case WM_DESTROY:
-		KillTimer(hWnd, 1);
-		PostQuitMessage(0);
-		return 0;
-	}
-	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
-}
+//#include <windows.h>
+//#include <random>
+//
+//std::uniform_int_distribution<int> uid{-29, 29};
+//std::default_random_engine dre;
+//
+//LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+//HINSTANCE g_hInst;
+//LPCTSTR lpszClass = TEXT("Timer");
+//
+//int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance
+//	, LPSTR lpszCmdParam, int nCmdShow)
+//{
+//	HWND hWnd;
+//	MSG Message;
+//	WNDCLASS WndClass;
+//	g_hInst = hInstance;
+//
+//	WndClass.cbClsExtra = 0;
+//	WndClass.cbWndExtra = 0;
+//	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+//	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+//	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+//	WndClass.hInstance = hInstance;
+//	WndClass.lpfnWndProc = WndProc;
+//	WndClass.lpszClassName = lpszClass;
+//	WndClass.lpszMenuName = NULL;
+//	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+//	RegisterClass(&WndClass);
+//
+//	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW,
+//		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+//		NULL, (HMENU)NULL, hInstance, NULL);
+//	ShowWindow(hWnd, nCmdShow);
+//
+//	while (GetMessage(&Message, NULL, 0, 0)) {
+//		TranslateMessage(&Message);
+//		DispatchMessage(&Message);
+//	}
+//	return (int)Message.wParam;
+//}
+//
+//LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+//{
+//	HDC hdc;
+//	PAINTSTRUCT ps;
+//	static RECT rt = { 100,100,400,120 };
+//	static TCHAR sTime[128];
+//	static int a;
+//	static int b;
+//
+//	switch (iMessage) {
+//	case WM_CREATE:
+//		SetTimer(hWnd, 1, 3000, NULL);
+//		SendMessage(hWnd, WM_TIMER, 1, 0);
+//		return 0;
+//	case WM_TIMER:
+//		a = uid(dre);
+//		b = uid(dre);
+//		wsprintf(sTime, TEXT("%d + %d = ?"), a, b);
+//		InvalidateRect(hWnd, &rt, TRUE);
+//		return 0;
+//	case WM_PAINT:
+//		hdc = BeginPaint(hWnd, &ps);
+//		TextOut(hdc, 100, 100, sTime, lstrlen(sTime));
+//		EndPaint(hWnd, &ps);
+//		return 0;
+//	case WM_DESTROY:
+//		KillTimer(hWnd, 1);
+//		PostQuitMessage(0);
+//		return 0;
+//	}
+//	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+//}
