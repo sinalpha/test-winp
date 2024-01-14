2ed2f5844235bde0a74679042687b1086da1f3ed
ssinalpha <hanhdhx65@gmail.com>
2024-01-15 03:25:04 +0900

    비트맵 완성

--- ch6/resource.h
+++ ch6/resource.h
@@ -4,6 +4,7 @@
 //
 #define IDR_ACCELERATOR1                101
 #define IDR_MENU1                       102
+#define IDB_BITMAP2                     104
 #define ID_40004                        40004
 #define ID_40005                        40005
 #define ID_40006                        40006
@@ -13,7 +14,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40011
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
