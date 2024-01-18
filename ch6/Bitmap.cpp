0e8d2bdb92200db27eb4a207037697d3eb90a348
ssinalpha <hanhdhx65@gmail.com>
2024-01-18 20:41:24 +0900

    RopMode2과제 완성!

--- ch6/Bitmap.cpp
+++ ch6/Bitmap.cpp
@@ -1,63 +1,63 @@
-#include <windows.h>
-#include "resource.h"
-
-LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
-HINSTANCE g_hInst;
-LPCTSTR lpszClass=TEXT("Bitmap");
-
-int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance
-		  ,LPSTR lpszCmdParam,int nCmdShow)
-{
-	HWND hWnd;
-	MSG Message;
-	WNDCLASS WndClass;
-	g_hInst=hInstance;
-	
-	WndClass.cbClsExtra=0;
-	WndClass.cbWndExtra=0;
-	WndClass.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);
-	WndClass.hCursor=LoadCursor(NULL,IDC_ARROW);
-	WndClass.hIcon=LoadIcon(NULL,IDI_APPLICATION);
-	WndClass.hInstance=hInstance;
-	WndClass.lpfnWndProc=(WNDPROC)WndProc;
-	WndClass.lpszClassName=lpszClass;
-	WndClass.lpszMenuName=NULL;
-	WndClass.style=CS_HREDRAW | CS_VREDRAW;
-	RegisterClass(&WndClass);
-
-	hWnd=CreateWindow(lpszClass,lpszClass,WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,
-		NULL,(HMENU)NULL,hInstance,NULL);
-	ShowWindow(hWnd,nCmdShow);
-	
-	while(GetMessage(&Message,0,0,0)) {
-		TranslateMessage(&Message);
-		DispatchMessage(&Message);
-	}
-	return Message.wParam;
-}
-
-#include "resource.h"
-LRESULT CALLBACK WndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
-{
-	HDC hdc,MemDC;
-	PAINTSTRUCT ps;
-	HBITMAP MyBitmap, OldBitmap;
-	switch(iMessage) {
-	case WM_PAINT:
-		hdc = BeginPaint(hWnd, &ps);
-		MemDC=CreateCompatibleDC(hdc);
-		MyBitmap=LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP2));
-		OldBitmap=(HBITMAP)SelectObject(MemDC, MyBitmap);
-		BitBlt(hdc, 0,0,1920,1280,MemDC,0,0,SRCCOPY);
-		SelectObject(MemDC,OldBitmap);
-		DeleteObject(MyBitmap);
-		DeleteDC(MemDC);
-		EndPaint(hWnd, &ps);
-		return 0;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		return 0;
-	}
-	return(DefWindowProc(hWnd,iMessage,wParam,lParam));
-}
+//#include <windows.h>
+//#include "resource.h"
+//
+//LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+//HINSTANCE g_hInst;
+//LPCTSTR lpszClass=TEXT("Bitmap");
+//
+//int APIENTRY WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance
+//		  ,LPSTR lpszCmdParam,int nCmdShow)
+//{
+//	HWND hWnd;
+//	MSG Message;
+//	WNDCLASS WndClass;
+//	g_hInst=hInstance;
+//	
+//	WndClass.cbClsExtra=0;
+//	WndClass.cbWndExtra=0;
+//	WndClass.hbrBackground=(HBRUSH)GetStockObject(WHITE_BRUSH);
+//	WndClass.hCursor=LoadCursor(NULL,IDC_ARROW);
+//	WndClass.hIcon=LoadIcon(NULL,IDI_APPLICATION);
+//	WndClass.hInstance=hInstance;
+//	WndClass.lpfnWndProc=(WNDPROC)WndProc;
+//	WndClass.lpszClassName=lpszClass;
+//	WndClass.lpszMenuName=NULL;
+//	WndClass.style=CS_HREDRAW | CS_VREDRAW;
+//	RegisterClass(&WndClass);
+//
+//	hWnd=CreateWindow(lpszClass,lpszClass,WS_OVERLAPPEDWINDOW,
+//		CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,CW_USEDEFAULT,
+//		NULL,(HMENU)NULL,hInstance,NULL);
+//	ShowWindow(hWnd,nCmdShow);
+//	
+//	while(GetMessage(&Message,0,0,0)) {
+//		TranslateMessage(&Message);
+//		DispatchMessage(&Message);
+//	}
+//	return Message.wParam;
+//}
+//
+//#include "resource.h"
+//LRESULT CALLBACK WndProc(HWND hWnd,UINT iMessage,WPARAM wParam,LPARAM lParam)
+//{
+//	HDC hdc,MemDC;
+//	PAINTSTRUCT ps;
+//	HBITMAP MyBitmap, OldBitmap;
+//	switch(iMessage) {
+//	case WM_PAINT:
+//		hdc = BeginPaint(hWnd, &ps);
+//		MemDC=CreateCompatibleDC(hdc);
+//		MyBitmap=LoadBitmap(g_hInst, MAKEINTRESOURCE(IDB_BITMAP2));
+//		OldBitmap=(HBITMAP)SelectObject(MemDC, MyBitmap);
+//		BitBlt(hdc, 0,0,1920,1280,MemDC,0,0,SRCCOPY);
+//		SelectObject(MemDC,OldBitmap);
+//		DeleteObject(MyBitmap);
+//		DeleteDC(MemDC);
+//		EndPaint(hWnd, &ps);
+//		return 0;
+//	case WM_DESTROY:
+//		PostQuitMessage(0);
+//		return 0;
+//	}
+//	return(DefWindowProc(hWnd,iMessage,wParam,lParam));
+//}
